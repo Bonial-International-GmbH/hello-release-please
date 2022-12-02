e57ed824f471d502a2dec1a266f216d7ecf1dae2
Martin Ohmann <martin.ohmann@bonial.com>
2022-12-02 14:53:22 +0100

    feat: add `--version` flag

--- main.go
+++ main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+// Populated during build.
+var version string
+var showVersion bool
 
 func main() {
+	flag.BoolVar(&showVersion, "version", false, "Display the version and exit")
+	flag.Parse()
+
+	if showVersion {
+		if version == "" {
+			version = "devel"
+		}
+
+		fmt.Printf("%s\n", version)
+		os.Exit(0)
+	}
+
 	fmt.Println("hello release-please")
 }
